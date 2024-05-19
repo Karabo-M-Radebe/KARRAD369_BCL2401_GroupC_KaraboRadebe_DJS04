b4e9643e19b56d17d0530b307519928bc44b87dd
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-05-19 18:22:11 +0200

    wenat ahead and created a constructor that has a shadow DOM (left it in closed mode )

--- components/bookpreviews.js
+++ components/bookpreviews.js
@@ -1,5 +1,11 @@
 class booksPreview extends HTMLElement {
-       
+    constructor() {
+        super(); //must call this to initiate parent class
+
+        const shadowRoot = this.attachShadow (init: {
+            mode: 'closed'
+        })
+    } 
 } 
 
 customElements.define( name: 'books-preview', booksPreview) 
