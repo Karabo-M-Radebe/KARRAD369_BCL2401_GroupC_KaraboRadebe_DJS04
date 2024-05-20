46a4ef285d5bd2c38e8ad56e2998daf65cc2b735
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-05-20 04:59:34 +0200

    added a shadowRoot to the innerHTML with the preview image and div responsible for the preview of the books

--- components/bookpreviews.js
+++ components/bookpreviews.js
@@ -5,7 +5,19 @@ class booksPreview extends HTMLElement {
         const shadowRoot = this.attachShadow (init: {
             mode: 'closed'
         })
+
+        this.shadowRoot.innerHTML = `
+        <img
+            class="preview__image"
+            src="${image}"
+        />
+        
+        <div class="preview__info">
+            <h3 class="preview__title">${title}</h3>
+            <div class="preview__author">${authors[author]}</div>
+        </div>
+        `
     } 
 } 
 
-customElements.define( name: 'books-preview', booksPreview) 
+customElements.define( name: 'books-preview', booksPreview)  
